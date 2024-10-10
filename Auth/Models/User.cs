c127af6f96f029d59a5cc6ee64652cd6fed81211
cristianqsanchez <cristianqsdev@gmail.com>
2024-10-10 03:39:39 -0500

    feat: add user model validations

--- Auth/Models/User.cs
+++ Auth/Models/User.cs
@@ -1,15 +1,37 @@
+using System.ComponentModel.DataAnnotations;
 namespace Auth.Models
 {
-    public class User
-    {
-        public required string Id { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-        public required string Email { get; set; }
-        public required string Address { get; set; }
-        public required string City { get; set; }
-        public required string Phone { get; set; }
-    }
+
+  public class User
+  {
+    [Required(ErrorMessage = "The field Id is required")]
+    [StringLength(15, ErrorMessage = "The Id field must not exceed 15 characters.")]
+    public required string Id { get; set; }
+
+    [Required(ErrorMessage = "The Username field is required")]
+    [StringLength(50, ErrorMessage = "The Username must not exceed 50 characters.")]
+    public required string Username { get; set; }
+
+    [Required(ErrorMessage = "The Password field is required")]
+    [StringLength(20, ErrorMessage = "The Password must not exceed 20 characters.")]
+    public required string Password { get; set; }
+
+    [Required(ErrorMessage = "The Email field is required")]
+    [StringLength(50, ErrorMessage = "The Email must not exceed 50 characters.")]
+    [EmailAddress(ErrorMessage = "The Email format is invalid.")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "The Address field is required")]
+    [StringLength(300, ErrorMessage = "The Address must not exceed 300 characters.")]
+    public required string Address { get; set; }
+
+    [StringLength(20, ErrorMessage = "The City must not exceed 20 characters.")]
+    public required string City { get; set; }
+
+    [StringLength(20, ErrorMessage = "The Phone number must not exceed 20 characters.")]
+    public required string Phone { get; set; }
+  }
+
 
     public class LoginRequest
     {
