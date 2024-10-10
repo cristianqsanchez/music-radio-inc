ef600578ee9e253da04a6c0231f9f7548e1705cd
cristianqsanchez <cristianqsdev@gmail.com>
2024-10-10 00:00:28 -0500

    feat: add album model

---
+++ Auth/Models/Album.cs
@@ -0,0 +1,10 @@
+namespace Auth.Models;
+public class Album
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Cover { get; set; }
+    public required string Artist { get; set; }
+    public required string ReleaseDate { get; set; }
+}
+
